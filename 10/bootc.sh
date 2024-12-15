e36c5df395a0714f4d4dd9b715c793a71611a5ec
imbev <imbev@protonmail.com>
2024-12-15 02:25:23 +0000

    Workaround selinux fstab bug

--- 10/bootc.sh
+++ 10/bootc.sh
@@ -4,6 +4,9 @@
 set -e
 
 
+dnf install -y selinux-policy-40.13.13-1.el10
+
+
 sed -i 's,ExecStart=/usr/bin/bootc update --apply --quiet,ExecStart=/usr/bin/bootc update --quiet,g' \
     /usr/lib/systemd/system/bootc-fetch-apply-updates.service
 
