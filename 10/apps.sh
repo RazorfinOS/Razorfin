6a4bfa3ef436b5ba53563f523fbc6076d27aaf3c
Isaac Beverly <imbev@protonmail.com>
2025-01-14 13:52:40 -0600

    [10] Add development group

--- 10/apps.sh
+++ 10/apps.sh
@@ -10,7 +10,8 @@ dnf install -y \
 
 
 dnf install -y \
-    distrobox
+    distrobox \
+    @development
 
 
 # Appimage
@@ -18,24 +19,6 @@ dnf install -y \
     fuse
 
 
-#dnf install -y \
-#    docker-ce \
-#    docker-ce-cli \
-#    containerd.io \
-#    docker-buildx-plugin \
-#    docker-compose-plugin
-
-export DOCKER_VERSION=27.3.1
-
-wget https://download.docker.com/linux/static/stable/x86_64/docker-${DOCKER_VERSION}.tgz && \
-    tar -xzf docker-${DOCKER_VERSION}.tgz && \
-    cp docker/* /usr/bin/
-
-wget https://download.docker.com/linux/static/stable/x86_64/docker-rootless-extras-${DOCKER_VERSION}.tgz && \
-    tar -xzf docker-rootless-extras-${DOCKER_VERSION}.tgz && \
-    cp docker-rootless-extras/* /usr/bin/
-
-    
 #dnf install -y \
 #    https://kojipkgs.fedoraproject.org//packages/bootc-gtk/0.3/1.el9/noarch/bootc-gtk-0.3-1.el9.noarch.rpm
 
