4268d0a390dd1926e9bd2afb108be4d8a4840dc1
imbev <imbev@protonmail.com>
2025-06-10 01:29:17 +0000

    Add steam udev rules

--- 10/hardware.sh
+++ 10/hardware.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env sh
 
-
 set -xeuo pipefail
 
+dnf install -y \
+    steam-devices
 
 dnf install -y \
     nvidia-open-kmod
 
-
 kver=$(cd /usr/lib/modules && echo * | awk '{print $1}')
 
-
 dracut -vf /usr/lib/modules/$kver/initramfs.img $kver
+
