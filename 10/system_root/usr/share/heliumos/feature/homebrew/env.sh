ddfe0f986cfa5f09a335eec2e0f3c2e39749d11b
Isaac Beverly <imbev@protonmail.com>
2025-06-15 20:20:31 -0500

    Remove C/C++ include paths and LD_LIBRARY_PATH from Homebrew env script

--- 10/system_root/usr/share/heliumos/feature/homebrew/env.sh
+++ 10/system_root/usr/share/heliumos/feature/homebrew/env.sh
@@ -5,9 +5,6 @@ if [ "$IN_DISTROBOX" = "0" ]; then
 
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
     export PATH=$PATH:$(brew --prefix)/bin/
-    export C_INCLUDE_PATH=$C_INCLUDE_PATH:$(brew --prefix)/include/
-    export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:$(brew --prefix)/include/
-    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(brew --prefix)/lib/
     export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:$(brew --prefix)/lib/pkgconfig/
 
 fi
