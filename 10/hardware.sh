94393970d4546437a96d74478dfcf41d14246084
Isaac Beverly <imbev@protonmail.com>
2025-06-27 16:13:28 -0500

    Use pre-release build of nvidia-open-kmod

--- 10/hardware.sh
+++ 10/hardware.sh
@@ -7,10 +7,10 @@ set -xeuo pipefail
 dnf install -y \
     steam-devices
 
-
 dnf install -y \
-    kmod-nvidia-open
-
+    https://build.almalinux.org/pulp/content/builds/AlmaLinux-Kitten-10-x86_64-39017-br/Packages/k/kmod-nvidia-open-575.64-1.el10.x86_64.rpm \
+    https://build.almalinux.org/pulp/content/builds/AlmaLinux-Kitten-10-x86_64-39017-br/Packages/n/nvidia-open-kmod-575.64-1.el10.x86_64.rpm
+    # nvidia-open-kmod
 
 kver=$(cd /usr/lib/modules && echo * | awk '{print $1}')
 
