dd8c7af8e2ea3c6bf8142d7020d3ebe46bb77917
Isaac Beverly <imbev@protonmail.com>
2025-06-27 18:16:51 -0500

    Use release package for nvidia-open-kmod

--- 10/hardware.sh
+++ 10/hardware.sh
@@ -8,9 +8,7 @@ dnf install -y \
     steam-devices
 
 dnf install -y \
-    https://build.almalinux.org/pulp/content/builds/AlmaLinux-Kitten-10-x86_64-39017-br/Packages/k/kmod-nvidia-open-575.64-1.el10.x86_64.rpm \
-    https://build.almalinux.org/pulp/content/builds/AlmaLinux-Kitten-10-x86_64-39017-br/Packages/n/nvidia-open-kmod-575.64-1.el10.x86_64.rpm
-    # nvidia-open-kmod
+    nvidia-open-kmod
 
 kver=$(cd /usr/lib/modules && echo * | awk '{print $1}')
 
